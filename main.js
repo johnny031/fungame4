dcb6f1d29b4093e2b94d92732681acc0608cf342
Johnny <johnny055238@gmail.com>
2020-01-26 23:16:59 +0800

    add adjust score feature

--- main.js
+++ main.js
@@ -55,11 +55,29 @@ function start() {
     var td_name = document.createElement("td");
     td_name.innerHTML = dict[i];
     td_name.className = "name" + i;
-    var td_score = document.createElement("td");
-    td_score.innerHTML = 0;
-    td_score.className = "score" + i;
+    var td_btn = document.createElement("td");
+    var btn_minus = document.createElement("button");
+    var icon_minus = document.createElement("i");
+    var div_score = document.createElement("div");
+    var btn_plus = document.createElement("button");
+    var icon_plus = document.createElement("i");
+
+    td_btn.style.position = "relative";
+    btn_minus.className = "minus";
+    icon_minus.className = "icon_minus fas fa-minus " + i;
+    div_score.innerHTML = 0;
+    div_score.className = "score" + i;
+    btn_plus.className = "plus";
+    icon_plus.className = "icon_plus fas fa-plus " + i;
+
+    btn_minus.append(icon_minus);
+    btn_plus.append(icon_plus);
+    td_btn.append(btn_minus);
+    td_btn.append(div_score);
+    td_btn.append(btn_plus);
+
     tr.append(td_name);
-    tr.append(td_score);
+    tr.append(td_btn);
     $("#score_board").append(tr);
   }
   $("#current_player").html($(".name0").html());
@@ -96,7 +114,15 @@ function clear_round() {
 
 document.addEventListener("click", event => {
   const element = event.target;
-  if (playing == true && element.className.slice(0, 3) === "btn") {
+  if (element.className.slice(0, 10) === "icon_minus") {
+    var adjust_minus = $(".score" + element.className.slice(-1)).html();
+    adjust_minus--;
+    $(".score" + element.className.slice(-1)).html(adjust_minus);
+  } else if (element.className.slice(0, 9) === "icon_plus") {
+    var adjust_plus = $(".score" + element.className.slice(-1)).html();
+    adjust_plus++;
+    $(".score" + element.className.slice(-1)).html(adjust_plus);
+  } else if (playing == true && element.className.slice(0, 3) === "btn") {
     current_words++;
     $("#words").html(words[current_words]);
     if (!isNaN(element.className.slice(-1))) {
@@ -137,7 +163,11 @@ $(".btn_skip").on("click", function() {
 });
 
 function return_to_zero() {
-  $('td[class^="score"]').each(function() {
+  $('div[class^="score"]').each(function() {
     $(this).html(0);
   });
+}
+
+function adjust() {
+  $(".minus, .plus").toggle();
 }
